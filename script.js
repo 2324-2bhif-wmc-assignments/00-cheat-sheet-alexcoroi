8bfcc234ee49bf549378bb6b5d9e8a96685c8cea
alexcoroi <root@DESKTOP-2VF7U66>
2024-02-07 18:48:45 +0100

    Added code snippets and improved cheatsheet

---
+++ script.js
@@ -0,0 +1,24 @@
+
+function removeSpacesFromCodeSnippets() {
+    [].forEach.call(document.querySelectorAll('code'), function($code) {
+        var lines = $code.textContent.split('\n');
+
+        if (lines[0] === '')
+        {
+            lines.shift()
+        }
+
+        var matches;
+        var indentation = (matches = /^[\s\t]+/.exec(lines[0])) !== null ? matches[0] : null;
+        if (!!indentation) {
+            lines = lines.map(function(line) {
+                line = line.replace(indentation, '')
+                return line.replace(/\t/g, '    ')
+            });
+
+            $code.textContent = lines.join('\n').trim();
+        }
+    });
+}
+removeSpacesFromCodeSnippets();
+hljs.highlightAll();
